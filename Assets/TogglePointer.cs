d6de44891151160af0254e6300e65c7c2f3c853b
Peter Loef <peterloef@gmail.com>
2020-02-05 13:25:03 +0100

    Initial commit

---
+++ Assets/TogglePointer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TogglePointer : MonoBehaviour {
+
+    public VRTK_Pointer pointer;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<VRTK_ControllerEvents>().TouchpadPressed += PressedTouchpad;
+        GetComponent<VRTK_ControllerEvents>().TouchpadReleased += ReleasedTouchpad;
+    }
+	
+    private void PressedTouchpad(object sender, ControllerInteractionEventArgs e)
+    {
+        pointer.Toggle(false);
+    }
+
+    private void ReleasedTouchpad(object sender, ControllerInteractionEventArgs e)
+    {
+        pointer.Toggle(true);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
